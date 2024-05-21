11a08156da378579893097cf603aa262301fbac3
Adwaitsharadmore <amore14@asu.edu>
2024-05-21 23:14:01 +0530

    Initial commit

---
+++ script1.py
@@ -0,0 +1,8 @@
+
+# Random Python Script
+
+def lrmhlviakt():
+    print("This is a random function called lrmhlviakt.")
+
+if __name__ == "__main__":
+    lrmhlviakt()
---
+++ script2.py
@@ -0,0 +1,8 @@
+
+# Random Python Script
+
+def xpgsateydj():
+    print("This is a random function called xpgsateydj.")
+
+if __name__ == "__main__":
+    xpgsateydj()
